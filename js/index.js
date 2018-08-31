bc754fc1d80d543774bc9c0478e48f5ca6c7c74e
Kendra Williams <kmonique.williams@gmail.com>
2018-08-30 20:04:29 -0400

    3 events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
 // Your code goes here
+//grab items
+const anchorElements = document.querySelectorAll("a");
+const headerElement = document.querySelector("header");
+const navElements = document.querySelectorAll("nav");
+const paraElements = document.querySelectorAll("p");
+const imgElements = document.querySelectorAll("img");
+const h1Element = document.querySelector("h1");
+
+//function declorations for changes
+const reset = function () {
+    this.style = "";
+    console.log(event);
+}
+
+const displayNone = function (){
+    this.style.display = "none";
+    console.log(event);
+}
+
+const colorLavender = function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.style.backgroundColor = "black";
+    this.style.color = "white";
+    this.style.padding = "10px";
+    console.log(event);
+}
+
+const highlightYellow = function (event){
+    this.style.backgroundColor = "yellow";
+    console.log(event);
+}
+const highlightOrange = function (event){
+    event.stopPropagation();
+    this.style.backgroundColor = "orange";
+    this.style.padding = "30px";
+    console.log(event);
+}
+
+//event listeners for actions
+anchorElements.forEach ((anchor) => {
+    anchor.addEventListener("mouseover", colorLavender);
+    anchor.addEventListener("mouseleave", reset);
+})
+
+headerElement.addEventListener("dblclick", highlightYellow);
+
+navElements.forEach ((nav) => {
+    nav.addEventListener("dblclick", highlightOrange);
+})
